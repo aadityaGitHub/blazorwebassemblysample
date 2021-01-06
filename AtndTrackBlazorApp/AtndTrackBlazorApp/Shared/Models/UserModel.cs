ba234e0b9d2f90f33fdaf4a2323314d098169b0e
Aditya <infotoadi@gmail.com>
2021-01-06 14:53:36 +0530

    User page added

---
+++ AtndTrackBlazorApp/AtndTrackBlazorApp/Shared/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtndTrackBlazorApp.Shared.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int RoleId { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
